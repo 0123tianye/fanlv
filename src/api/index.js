6ddaba2b4fa4389f54f915b1d1f07d494a81a89f
fanlv <147700983+0123tianye@users.noreply.github.com>
2024-08-21 15:29:25 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -55,9 +55,9 @@ export const getHitokoto = async () => {
 const fetch = require('node-fetch');
 
 // 获取高德地理位置信息
-const getAdcode = async (apiKey) => {
+const getAdcode = async (b5ead205705615c795e89f9b46a8c62c) => {
   try {
-    const res = await fetch(`https://restapi.amap.com/v3/ip?key=${apiKey}`);
+    const res = await fetch(`https://restapi.amap.com/v3/ip?key=${b5ead205705615c795e89f9b46a8c62c}`);
     const data = await res.json();
     return data.adcode;  // 返回城市编码
   } catch (error) {
@@ -66,10 +66,10 @@ const getAdcode = async (apiKey) => {
 };
 
 // 获取高德地理天气信息
-const getWeather = async (apiKey, city) => {
+const getWeather = async (b5ead205705615c795e89f9b46a8c62c) => {
   try {
     const res = await fetch(
-      `https://restapi.amap.com/v3/weather/weatherInfo?key=${apiKey}&city=${city}`
+      `https://restapi.amap.com/v3/weather/weatherInfo?key=${b5ead205705615c795e89f9b46a8c62c}&city=${city}`
     );
     const data = await res.json();
     return data;  // 返回天气信息
@@ -90,10 +90,10 @@ const getOtherWeather = async () => {
 };
 
 // 示例使用
-const apiKey = 'your_amap_api_key';  // 替换为有效的 API Key
+const apiKey = 'b5ead205705615c795e89f9b46a8c62c';  // 替换为有效的 API Key
 
 (async () => {
-  const adcode = await getAdcode(apiKey);
+  const adcode = await getAdcode(b5ead205705615c795e89f9b46a8c62c);
   console.log('Adcode:', adcode);
 
   const weather = await getWeather(apiKey, adcode);
